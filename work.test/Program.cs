a94da594dd52d7c677b8a493aec6a3680b7557fb
Junwei home PC <320095019@qq.com>
2021-05-16 18:14:18 +0800

    move file to work project

---
+++ work.test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace work.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
